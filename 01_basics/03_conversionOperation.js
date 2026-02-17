75a0b9304a9a22739f5fbea88d7c478dd49f4cd1
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-17 18:05:12 +0000

    conversion on datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score ="23abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNUmber=Number(score)
+
+// console.log(typeof(valueInNUmber));
+// console.log(valueInNUmber);
+
+// "33" =>33
+// "33abc" =>NaN
+// true =>1; false =>0
+
+let isLoggedIn = ""
+
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>true 0=>fales
+//""=>fales
+// "vinay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
