019d9970afca9e4f3701bb787b5fa5346f6c47ac
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-17 18:33:49 +0000

    Add conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,50 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
+
+// *****************************    Operation  **********************************
+
+let value =3
+
+let negValue=-value
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+
+let str1 = "Vinay"
+let str2 =  " Kumar"
+
+let str3 =str1 +str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
 
