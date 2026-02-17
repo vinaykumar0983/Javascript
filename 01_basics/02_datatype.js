460aeeb825422eac62e7b1f138aa987253c9558d
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-17 07:04:43 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3+3)
+
+let name="Vinay"
+let age=20
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fales
+// null => standalone value
+// undefined => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
+
+
