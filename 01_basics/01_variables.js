aca80dbeca0c3166bb1b0454dc2cb1211ace75e6
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-17 07:04:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=1234
+let accountEmail="vinayjeet92@gmail.com"
+var accountPassword="1234"
+accountCity = "Gonda"
+
+let accountstate;
+
+// accountId = 123  Not allowed
+
+//console.log(accountId);
+
+accountEmail="krishna92@gmail.com"
+accountPassword="1234444"
+accountCity = "Lacknow"
+
+/* 
+Prefer not to use Var
+Because of issue in block scope and functional scope
+
+*/
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountstate])
