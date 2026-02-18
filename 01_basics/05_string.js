790fbc3d51313f516a1b4f08e902d1c8c8efa9e5
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-18 05:49:41 +0000

    Strings and its function

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name="Vinay"
+const repocount = 50;
+
+//console.log(name + repocount + " value"); outdated not prefered
+
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('Vinay-dev-com')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+//console.log(gameName.charAt(4));
+//console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.substring(-4,8)
+//console.log(anotherString);
+
+
+
+const newStringOne="  Vinay     "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url="https://vinay.com/vinay%20kumar"
+
+//console.log(url.replace('%20','-'));
+
+//console.log(url.includes('inay'))
+
+console.log(gameName.split('-'));
+
