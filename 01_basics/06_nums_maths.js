a5d71c08b78833b71bf43555c81b1d5b45b74880
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-18 06:41:49 +0000

    Math and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,38 @@
+const score = 400;
+//console.log(score);
+
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8932
+//console.log(otherNumber.toPrecision(4));
+
+const hundreads=1000000
+//console.log(hundreads.toLocaleString('en-IN'));
+
+
+
+//======================== Maths =====================================
+
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(4.56));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(6.9));
+// console.log(Math.pow(3,2));
+// console.log(Math.min(3,4,2,5,6));
+
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min)
+
+
+
+
