1582ea4c9ae150992045c4ce6c24c3df5e9c0067
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-18 04:37:16 +0000

    Data types summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,44 @@
+// Primitive Data type
+
+// 7 types : String ,Number ,Booolean, Null, Undefined, Symbol, BigInt
+
+const score=2000
+const scoreValue=23.43
+
+const isLoggedIn =false
+const outSideTemp =null
+let userEmail;
+
+
+const  id = Symbol('123')
+const  anotherId = Symbol('123')
+
+console.log(id==anotherId);  //fales
+console.log(anotherId);
+
+
+const bigNumber = 222222234444445677n
+
+
+
+
+
+// reference(Non primitive)
+
+///Array, Objects,Function
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:"Vinay",
+    age:23
+
+
+}
+
+const myFuunction=function(){
+    console.log("hello Word !");
+    
+}
+
+console.log(typeof bigNumber);
+
