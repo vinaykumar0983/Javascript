6875e13fef821fe2e38f37319988f786e48e19c9
vinaykumar0983 <vinayjeet92@gmail.com>
2026-02-19 16:26:19 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,50 @@
+// Arrays
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Vinay","Krishna","Shivam"]
+
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[2]);
+
+// Array methds
+
+// myArr.push(6)    Add element at the End of Array
+// myArr.push(7)
+// myArr.pop()      Remove element at the End
+// console.log(myArr);
+
+//myArr.unshift(9)  Add element at beggining
+//myArr.shift()     Remove element at beggining
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+// console.log(myArr.indexOf(19));  // Output -1 because 19 exit
+
+// const newArr = myArr.join()
+
+
+// console.log(newArr);  //typeof  String
+//console.log(myArr);
+
+//slice, splice
+
+// console.log("A ",myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B ",myArr);
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log("C ",myArr);
+
+// console.log(myn2);
+
+
+
+
+
+
+
