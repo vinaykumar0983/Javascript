c3998f69209d70676175ecd1002efe421cd2ebf0
vinaykumar0983 <vinayjeet92@gmail.com>
2026-03-20 13:15:45 +0000

    modification

---
+++ Events_Projects/6-CololChanging/chaiaurcode.js
@@ -0,0 +1,31 @@
+// Generate a random color
+
+const randomColor = function (){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+
+    return color
+};
+
+let intervalTd;
+const startChangingColor = function (){
+    if(!intervalTd){
+      intervalTd = setInterval(changeBgColor,1000);
+    }
+    
+    function changeBgColor(){
+      document.body.style.backgroundColor= randomColor();
+    }
+    
+};
+const stopChangingColor = function (){
+    clearInterval(intervalTd);
+    intervalTd = null;
+};
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
